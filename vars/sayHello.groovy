0ad14db19fd12a608faa19c06d7fd46ff7445450
Jithendra <jithendra.muppalla@cloudjournee.com>
2019-04-10 12:24:53 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,44 @@
-#!/usr/bin/env groovy
+def call(Map pipelineParams) {
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                   echo 'Hi Sujith'
+                }
+            }
+
+            stage('build') {
+                steps {
+                    echo 'Hi Cloudjournee'
+                }
+            }
+
+            stage ('test') {
+                steps {
+                    echo 'Hi jeeth'
+                }
+            }
+
+            stage('deploy developmentServer'){
+                steps {
+                    echo 'Hi sujith'
+                }
+            }
+
+            stage('deploy staging'){
+                steps {
+                    echo 'Hi Jithendra'
+                }
+            }
+
+            stage('deploy production'){
+                steps {
+                    echo 'Hi Hitesh'
+                }
+            }
+        }
+       
+    }
 }
-
